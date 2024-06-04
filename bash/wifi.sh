6c2d7df2373396693ecc43299b56924e975ad6fb
azsão <152450334+azsao@users.noreply.github.com>
2024-06-04 17:08:36 -0400

    shell
    added some stuff related to the installation + running commands directly from path

---
+++ bash/wifi.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+RUN_MAR="~/.pswdcnt/module/store"
+
+$RUN_MAR
--- installation.sh
+++ installation.sh
@@ -9,3 +9,7 @@ mv module/store.go ~/.pswdcnt/module/
 
 go build ~/.pswdcnt/module/store.go
 rm -r ~/.pswdcnt/module/store.go
+
+sudo mv bash/wifi.sh /usr/local/bin/wifi
+sudo chmod +x /usr/local/bin/wifi
+
